3ffe2f5bcab7f4139663577b5c639b3d2257f350
deniseurbanija <deniseurbanija1@gmail.com>
2025-03-05 12:25:17 -0300

    feat(login): mobile styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useLogin } from "@/hooks/useLogin";
 import Link from "next/link";
 import { useState } from "react";
+import Icon from "/public/paisabank-icon.svg";
 
 export default function LoginPage() {
   const [loginForm, setLoginForm] = useState({ email: "", password: "" });
@@ -17,35 +18,18 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <div className="w-full max-w-[400px] flex flex-col items-center">
-        {/* Logo */}
-        <div className="mb-2">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            'file:///C:/Users/denis/Downloads/Frame%20263.svg'
-            <path
-              d="..."
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+    <div className="min-h-screen flex flex-col items-center justify-center p-4 gap-44">
+      <div className="space-y-8 w-full">
+        <div className=" max-w-[400px] flex flex-col gap-4 items-center">
+          <Icon className="mb-2" />
+          <h1 className="text-[#0066FF] text-4xl font-medium mb-1">PaisBank</h1>
+          <p className="text-[#6B7280] text-sm mb-8">
+            Comienza a manejar tu vida financiera
+          </p>
         </div>
 
-        <h1 className="text-[#0066FF] text-4xl font-medium mb-3">PaisBank</h1>
-        <p className="text-[#6B7280] text-sm mb-8">
-          Comienza a manejar tu vida financiera
-        </p>
-
         {/* Form */}
-        <div className="w-96 space-y-6">
+        <div className="w-full space-y-5 ">
           <div className="space-y-1">
             <label className="text-[#334154] text-sm font-medium">Email</label>
             <input
@@ -53,12 +37,12 @@ export default function LoginPage() {
               name="email"
               placeholder="Ingresa tu email"
               onChange={handleChange}
-              className="w-full h-12 px-4  rounded-lg bg-white text-[#374151] placeholder:text-[#AAAAAA] focus:outline-none focus:border-[#0066FF] focus:ring-1 focus:ring-[#0066FF]"
+              className="w-full h-12 px-4 border border-gray-200 rounded-lg bg-white text-[#374151] placeholder:text-[#AAAAAA] focus:outline-none focus:border-[#0066FF] focus:ring-1 focus:ring-[#0066FF]"
             />
           </div>
 
           <div className="space-y-1">
-            <label className="text-[#334154] text-sm font-medium ">
+            <label className="text-[#334154] text-sm font-medium">
               Contraseña
             </label>
             <input
@@ -66,11 +50,11 @@ export default function LoginPage() {
               name="password"
               placeholder="Ingresa tu contraseña"
               onChange={handleChange}
-              className="w-full h-12  px-4 rounded-lg  bg-white text-[#374151] placeholder:text-[#AAAAAA] focus:outline-none focus:border-[#0066FF] focus:ring-1 focus:ring-[#0066FF]"
+              className="w-full h-12 px-4 border border-gray-200 rounded-lg bg-white text-[#374151] placeholder:text-[#AAAAAA] focus:outline-none focus:border-[#0066FF] focus:ring-1 focus:ring-[#0066FF]"
             />
           </div>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 mt-2">
             <input
               type="checkbox"
               id="remember"
@@ -81,17 +65,16 @@ export default function LoginPage() {
             </label>
           </div>
         </div>
-
-        {/* Button */}
-        <Link href="/home" className="w-full mt-8">
-          <button
-            onClick={handleSubmit}
-            className="w-full h-12 bg-[#0066FF] hover:bg-[#0052CC] text-white rounded-lg font-medium transition-colors"
-          >
-            Ingresar
-          </button>
-        </Link>
       </div>
+      {/* Button */}
+      <Link href="/home" className="w-full mt-8">
+        <button
+          onClick={handleSubmit}
+          className="w-full h-12 bg-[#0066FF] hover:bg-[#0052CC] text-white rounded-lg font-medium transition-colors"
+        >
+          Ingresar
+        </button>
+      </Link>
     </div>
   );
 }
