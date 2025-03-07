13da9237d0cf66289bfdb07eee9260c067d83f39
deniseurbanija <deniseurbanija1@gmail.com>
2025-03-06 22:48:54 -0300

    chore(backend): enable CORS for all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     credentials: true,
   });
 
