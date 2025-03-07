aa64b1da04b320844d0dde82ea4f35d96ac2b969
deniseurbanija <deniseurbanija1@gmail.com>
2025-03-06 22:17:45 -0300

    Revert "refactor(frontend): update import paths for SVG icons using alias"

--- frontend/src/components/ui/transaction-item.tsx
+++ frontend/src/components/ui/transaction-item.tsx
@@ -1,6 +1,6 @@
-import ArrowUp from "@/public/ArrowUp.svg";
-import ArrowDown from "@/public/ArrowDown.svg";
-import TwoArrows from "@/public/TwoArrows.svg";
+import ArrowUp from "/public/ArrowUp.svg";
+import ArrowDown from "/public/ArrowDown.svg";
+import TwoArrows from "/public/TwoArrows.svg";
 
 interface TransactionItemProps {
   title: string;
